206482f64483c9f1a3be9cbcd254afbdedda8d7b
Cosme Barbosa <cosmeg5@hotmail.com>
2020-08-30 21:57:32 -0300

    fim portifolio

--- js/script.js
+++ js/script.js
@@ -4,13 +4,91 @@ const filterContainer = document.querySelector(".portifolio-filter"),
       totalFilterBtn = filterBtns.length,
       portifolioItens = document.querySelectorAll(".portifolio-item"),
       totalPortifolioItens = portifolioItens.length;
-      console.log(totalPortifolioItens)
-    
-
 
       for(let index = 0; index<totalFilterBtn; index++) {
           filterBtns[index].addEventListener("click", function(){
               filterContainer.querySelector(".active").classList.remove("active");
               this.classList.add("active");
+
+              const filterValue = this.getAttribute("data-filter");
+              for (let k = 0; k < totalPortifolioItens; k++) {
+                  if(filterValue === portifolioItens[k].getAttribute("data-category")) {
+                    portifolioItens[k].classList.remove("hide")
+                      portifolioItens[k].classList.add("show");
+                  } else {
+                    portifolioItens[k].classList.remove("show");
+                    portifolioItens[k].classList.add("hide");
+                  }
+
+                  if(filterValue == "all") {
+                    portifolioItens[k].classList.remove("hide")
+                      portifolioItens[k].classList.add("show");  
+                  }
+              }
           });
       }
+
+
+// Portifolio Lightbox
+
+const lightbox = document.querySelector(".lightbox"),
+lightboxImg = lightbox.querySelector(".lightbox-img"),
+lightboxText = lightbox.querySelector(".caption-text"),
+lightboxCounter = lightbox.querySelector(".caption-count");
+let itemIndex = 0;
+
+for(let i = 0; i<totalPortifolioItens; i++) {
+    portifolioItens[i].addEventListener("click", function() {
+        itemIndex = i;
+        changeItem();
+        toggleLightbox();
+    });
+}
+
+const $prevItem = lightbox.querySelector(".prev-item");
+const $nextItem = lightbox.querySelector(".next-item");
+const $closeItem = lightbox.querySelector(".fa-close");
+
+lightbox.addEventListener("click",function(event){
+   if(event.target === $closeItem) {
+    toggleLightbox();
+   }
+});
+
+function toggleLightbox() {
+    lightbox.classList.toggle("open");
+}
+
+$prevItem.addEventListener("click", function(){
+    if(itemIndex === 0) {
+        itemIndex = totalPortifolioItens-1;
+    } else {
+        itemIndex--;
+    }
+    changeItem();
+});
+
+$prevItem.addEventListener("click", function(){
+    if(itemIndex === 0) {
+        itemIndex = totalPortifolioItens-1;
+    } else {
+        itemIndex--;
+    }
+    changeItem();
+});
+
+$nextItem.addEventListener("click", function(){
+    if(itemIndex === totalPortifolioItens-1) {
+        itemIndex = 0;
+    } else {
+        itemIndex++;
+    }
+    changeItem();
+});
+
+function changeItem() {
+    imgSrc = portifolioItens[itemIndex].querySelector(".portifolio-img img").getAttribute("src");
+    lightboxImg.src = imgSrc;
+    lightboxText.innerHTML = portifolioItens[itemIndex].querySelector("h4").innerHTML;
+    lightboxCounter.innerHTML = (itemIndex+1) + " of " + totalPortifolioItens;
+}
